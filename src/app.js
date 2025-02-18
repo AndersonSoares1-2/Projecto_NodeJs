378ca4f136ed5d0f3261bb85d028766e8024169f
Anderson Soares <anderson.soares@tistech.co.ao>
2025-02-18 10:36:04 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ const cores = [
 ];
 
 function buscarCorPeloId(id) {
-  const cor = cores.filter(cor => cor.id == id);
+  const cor = cores.filter((cor) => cor.id == id);
+  return cor;
+}
+
+function buscarIndexCor(id) {
+  const cor = cores.findIndex((cor) => cor.id == id);
   return cor;
 }
 app.get("/", (req, res) => {
@@ -27,6 +32,16 @@ app.get("/cores/:id", (req, res) => {
 app.post("/cores", (req, res) => {
   cores.push(req.body);
   res.status(201).send("Cor adicionada com sucesso!");
+});
+
+app.delete("/cores/:id", (req, res) => {
+  cores.splice(buscarIndexCor(req.params.id), 1);
+  res.send("Elemento eliminado com sucesso!");
+});
+
+app.put("/cores/:id", (req, res) => {
+  cores[buscarIndexCor(req.params.id)] = req.body;
+  res.send("Elemento atualizado com sucesso!");
 });
 
 export default app;
